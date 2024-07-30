03039e644f3beaa5c4adccf42302ed45f359acc6
Hugh O'Brien <github@hughobrien.ie>
2024-07-30 00:56:13 -0400

    tweak build script

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ set -xeou pipefail
 version="$(jq -r .version <manifest.json)"
 inputs=(icons manifest.json service_worker.js popup.html popup-animation.gif)
 output="kagi_chrome_${version}.zip"
+unpacked_dir="unpacked"
 
 rm -f "$output"
 zip -r "$output" "${inputs[@]}"
-unzip -d unpacked "$output" # test from this dir to ensure full asset capture
+
+# test from this dir to ensure full asset capture
+rm -rf "$unpacked_dir"
+unzip -d "$unpacked_dir" "$output"
